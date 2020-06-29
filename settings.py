cb284bdd6b76d8195ba6f724841bdee051e16b7b
Radek Kaczmarek <radoslaw.kaczmarek@mobica.com>
2020-06-29 13:35:38 +0200

    Initial commit

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """Klasa do przechowywania ustawień gry"""
+
+    def __init__(self):
+        """Inicjalizacja ustawień gry."""
+        # Ustawienia ekranu
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (200, 200, 200)
